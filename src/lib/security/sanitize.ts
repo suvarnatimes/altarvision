5a2e803e5b8737f18f79655dbb30af9d6897870a
AltarVision Admin <altarvision122@gmail.com>
2026-06-28 17:22:09 +0530

    fix: remove forward slash escaping in sanitizeInput to prevent breaking image/video URLs

--- src/lib/security/sanitize.ts
+++ src/lib/security/sanitize.ts
@@ -5,8 +5,7 @@ export function sanitizeInput(input: string): string {
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
     .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#x27;")
-    .replace(/\//g, "&#x2F;");
+    .replace(/'/g, "&#x27;");
 }
 
 export function sanitizeObject<T>(obj: T): T {
